d94e84d44e14db80b39527bcc3181392f3703f82
Keisha <ktm225@lehigh.edu>
2022-04-14 21:35:09 -0400

    Assignment 4

---
+++ Bis-044-MODIKEISHA/Assignment 4.R
@@ -0,0 +1,37 @@
+# install the "readxl" package:
+if(!require("readxl")) install.packages("readxl")
+library(readxl)
+
+# install the "tidyverse" package:
+if(!require("tidyverse")) install.packages("tidyverse")
+library(tidyverse)
+
+attach(dataset1)
+
+
+df$Title <- replace(df$Title, df$Title=="Aldermen", "Alderman")
+df$Title <- replace(df$Title, df$Title=="Clerk", "Clerk of Court")
+df$Title <- replace(df$Title, df$Title=="Constable(s)", "Constable")
+df$Title <- replace(df$Title, df$Title=="Council Member at Large", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Council Member I", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Council Member II", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Council Member III", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Council Member(s)", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Councilman", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Councilman at Large", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Councilmember", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Councilmember at Large", "Council Member")
+df$Title <- replace(df$Title, df$Title=="Councilmen", "Council Member")
+
+
+df$Title <- replace(df$Title, df$Title=="Judge, Court of Appeal", "Judge")
+df$Title <- replace(df$Title, df$Title=="Judge, Family Court", "Judge")
+df$Title <- replace(df$Title, df$Title=="Justice of the Peace, Parishwide", "Justice of the Peace")
+df$Title <- replace(df$Title, df$Title=="Justice of the Peace(s)", "Justice of the Peace")
+
+df$Title <- replace(df$Title, df$Title=="Mayor-President", "Mayor")
+
+
+
+results <- count(df,df$Title)
+View(results)
